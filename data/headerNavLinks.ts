cda88f4b5838031145b10bd27f6c05a40394e739
Ta Ngoc Minh <86149296+ngocminhta@users.noreply.github.com>
2023-12-07 15:13:21 +0700

    Update headerNavLinks.ts

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -3,6 +3,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   { href: '/about', title: 'About' },
+  { href: 'https://ngocminhta.github.io/', title: 'Portfolio' },
 ]
 
 export default headerNavLinks
